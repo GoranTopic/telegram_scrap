c41a24fb3ca0f98186ed91feac4c25db0b673e25
Goran Topic <telegonicaxx@live.com>
2023-10-13 16:50:33 -0700

    re wrote main.js without slavery

--- src/main.js
+++ src/main.js
@@ -5,109 +5,95 @@ import axios from 'axios';
 import generateToken from './utils/generateToken.js';
 import Storage from 'storing-me'
 import RandomUserAgent from 'random-useragent';
-import Slavery from 'slavery-js';
 
 // get the endpoint
 let endpoint = 'https://lugarvotacion.cne.gob.ec/CneApiWs/api/ConsultaVotacionDomicilioElectoral2021'
 
-// salve
-Slavery({
-    numberOfSlaves: 200,
-    host: 'localhost',
-    port: 3000,
-}).master( async master => {
 
-    let cedula_prefix = process.argv[2];
-    // let get the phone number from the params passed
-    console.log('reading cedulas starting with: ', cedula_prefix);
-    if(!cedula_prefix){
-        console.log('Please enter a number from 01 - 24 or 30');
-        process.exit(1);
+let cedula_prefix = process.argv[2];
+// let get the phone number from the params passed
+console.log('reading cedulas starting with: ', cedula_prefix);
+if(!cedula_prefix){
+    console.log('Please enter a number from 01 - 24 or 30');
+    process.exit(1);
+}
+
+// get proxies
+let proxies = new proxyRotator('./storage/proxies/proxyscrape_premium_http_proxies.txt', {
+    returnAs: 'object',
+});
+
+// read lines from and format it 
+let cedulas_dob = fs
+    .readFileSync(`./storage/cedulas/cedula_dob_${cedula_prefix}.txt`, 'utf8')
+    .split('\n')
+    .map(cedula => ({ cedula: cedula.split(',')[0], dob: cedula.split(',')[1] }));
+
+// traslat date of birth to format DD/MM/YYYY from 1938-06-14T00:00:00.000000000Z
+cedulas_dob = cedulas_dob
+    .map(cedula => ({ cedula: cedula.cedula, dob: cedula.dob.split('T')[0].split('-').reverse().join('') }));
+
+console.log('making storage...');
+let storage = new Storage({ 
+    type: 'json',
+    storagePath: `./storage/records/`,
+    keyValue: true,
+});
+let store = await storage.open(`records_${cedula_prefix}`);
+console.log('storage done');
+
+console.log('making checklist...');
+let cedula_checklist = new Checklist(cedulas_dob, { 
+    name: `cedulas_dob_${cedula_prefix}`,
+    path: './storage/checklists/',
+    enqueue: false,
+    recalc_on_check: false,
+    save_every_check: 1,
+});
+console.log('checklist done');
+
+// set intevale to make requests every 1 second
+let interval = setInterval(() => {
+    // check if all cedulas have been checked
+    if (cedula_checklist.isDone()) {
+        console.log('all cedulas checked');
+        clearInterval(interval);
+        return;
     }
-
-    // get proxies
-    let proxies = new proxyRotator('./storage/proxies/proxyscrape_premium_http_proxies.txt', {
-        returnAs: 'object',
-    });
-
-    // read lines from and format it 
-    let cedulas_dob = fs
-        .readFileSync(`./storage/cedulas/cedula_dob_${cedula_prefix}.txt`, 'utf8')
-        .split('\n')
-        .map(cedula => ({ cedula: cedula.split(',')[0], dob: cedula.split(',')[1] }));
-
-    // traslat date of birth to format DD/MM/YYYY from 1938-06-14T00:00:00.000000000Z
-    cedulas_dob = cedulas_dob
-        .map(cedula => ({ cedula: cedula.cedula, dob: cedula.dob.split('T')[0].split('-').reverse().join('') }));
-
-    console.log('making storage...');
-    let storage = new Storage({ 
-        type: 'json',
-        storagePath: `./storage/records/`,
-        keyValue: true,
-    });
-    let store = await storage.open(`records_${cedula_prefix}`);
-    console.log('storage done');
-
-    console.log('making checklist...');
-    let cedula_checklist = new Checklist(cedulas_dob, { 
-        name: `cedulas_dob_${cedula_prefix}`,
-        path: './storage/checklists/',
-        enqueue: false,
-        recalc_on_check: false,
-        save_every_check: 1,
-    });
-    console.log('checklist done');
-
+    // get the next proxy and cedula to check
     let proxy = proxies.next();
     let cedula = cedula_checklist.next();
     let token = generateToken();
     let userAgent = RandomUserAgent.getRandom();
+    // make the request
+    make_request(proxy, cedula, token, userAgent);
+}, 0.1);
 
-    while(cedula) {
-        console.log(`awaiting for slave...`);
-        let slave = await master.getIdle();
-        // send cedula to slave
-        slave.run({proxy, cedula, token, userAgent})
-            .then( async ({result, cedula, proxy}) => {
-                await store.push(cedula.cedula, result);
-                cedula_checklist.check(cedula);
-                console.log(`cedula ${cedula.cedula} checked. ${cedula_checklist.valuesCount()}/${cedula_checklist._missing_values.length} `);
-            }).catch( error => {
-                console.log(`[${proxy.ip}][${cedula.cedula}] error`);
-            })
-        // update variables
-        proxy = proxies.next();
-        cedula = cedula_checklist.next();
-        token = generateToken();
-        userAgent = RandomUserAgent.getRandom();
-    }
-}).slave( async ({proxy, cedula, token, userAgent}) => {
-    let response;
-    try {
-        response = await axios.post(endpoint, {
-            "cedula": cedula.cedula,
-            "nombre": cedula.dob,
-            "ip": proxy.ip,
-            "recaptcharesponse": token,
-            headers: {
-                'User-Agent': userAgent
-            }
-        }, {
-            proxy: {
-                host: proxy.ip, 
-                port: proxy.port,
-                protocol: 'http'
-            }
-        });
-        console.log(`[${proxy.ip}][${cedula.cedula}] querying...${response.status}`);
-        return ({result: response.data, cedula, proxy});
-    } catch (e) {
-        // if error is 403
-        if (e.response.status === 403 ) 
-            console.log(`[${proxy.ip}][${cedula.cedula}] querying...${e.response.status}`);
-        throw e;
-    }
-});
 
+let make_request = (proxy, cedula, token, userAgent) => 
+    // make the request
+    axios.post(endpoint, {
+        "cedula": cedula.cedula,
+        "nombre": cedula.dob,
+        "ip": proxy.ip,
+        "recaptcharesponse": token,
+        headers: {
+            'User-Agent': userAgent
+        }
+    }, {
+        proxy: {
+            host: proxy.ip, 
+            port: proxy.port,
+            protocol: 'http'
+        }
+    }).then( async result  => {
+        await store.push(cedula.cedula, result);
+        cedula_checklist.check(cedula);
+        console.log(`cedula ${cedula.cedula} checked. ${cedula_checklist.valuesCount()}/${cedula_checklist._missing_values.length} `);
+    }).catch( error => {
+        if (error.response.status === 403 ) 
+            console.log(`[${proxy.ip}][${cedula.cedula}] querying...${error.response.status}`);
+        else 
+            console.log(`[${proxy.ip}][${cedula.cedula}] error`);
+    })
 
